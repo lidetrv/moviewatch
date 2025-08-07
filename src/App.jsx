35bb5a40fa903915becf41b9eab74d005a882fa1
lidetu <lidetutadesse6@.com>
2025-08-07 17:59:51 +0300

    Spinner error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Spinner from './spinner.jsx';
+import Spinner from './Spinner.jsx';
 import Search from './components/Search.jsx';
 import MovieCard from './components/MovieCard.jsx';
 import { useDebounce } from 'react-use';
